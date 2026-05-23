47b189816fde8c3164c6d1446a6d64325ff97fa2
sahillatake <latakesahil50@gmail.com>
2026-05-24 02:12:42 +0530

    Update app.py

--- test-app/app.py
+++ test-app/app.py
@@ -1,3 +1,11 @@
+import os
+
+password = "admin123"
+
 user_input = input("Enter ID: ")
 
-print(user_input)
+query = "SELECT * FROM users WHERE id=" + user_input
+
+os.system("ping " + user_input)
+
+print(query)
